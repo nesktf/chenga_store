6305f622534c29778b2d8204b506ff2cbf0e6001
nesktf <nesktf@proton.me>
2025-02-10 23:32:12 -0300

    404 funnies

--- app/app.lua
+++ app/app.lua
@@ -1,4 +1,5 @@
 local lapis = require("lapis")
+local config = require("util").config
 local ngx = ngx
 
 local app = lapis.Application()
@@ -10,10 +11,11 @@ end
 app:enable("etlua")
 app.layout = require("views.layout")
 
-function app.handle_404()
+function app.handle_404(self)
   local api = ngx.var.uri:match("^(/api).+$")
 
   if (not api) then
+    self.page_title = config.site_name.." - 404"
     return { render="code_404" }
   end
 
